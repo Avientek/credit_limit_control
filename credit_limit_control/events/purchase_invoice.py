0392a3e6c4516c860423e3dc4e55cea49905e8c8
ajay <ajayn1991@gmail.com>
2023-07-04 16:27:10 +0530

    fix : Purchase Invoice, Purchase Order, Sales Invoice, Sales Order - Error Fix to get customer and supplier wise data

--- credit_limit_control/events/purchase_invoice.py
+++ credit_limit_control/events/purchase_invoice.py
@@ -1,25 +1,24 @@
 import frappe
 
 def before_submit(doc, method):
-    query="""   
+    query=frappe.db.sql(f"""
     SELECT
         SUM(pi.grand_total) AS grand_total,
         pii.purchase_order AS purchase_order
     FROM
         `tabPurchase Invoice` AS pi LEFT JOIN
         `tabPurchase Invoice Item` AS pii ON pii.parent=pi.name
-
     WHERE
-        pi.docstatus=1 AND pii.purchase_order IS NULL
+        pi.docstatus=1 AND pii.purchase_order IS NULL AND pi.supplier = '{doc.supplier}'
     GROUP BY
         pi.supplier,pii.purchase_order
-        """
-    amount= frappe.db.sql(f"{query}", as_dict=True)
-    supplier = frappe.get_doc('Supplier', doc.supplier)
+    """, as_dict = True)
 
+    supplier = frappe.get_doc('Supplier', doc.supplier)
     user = frappe.session.user
     role_assigned=frappe.db.get_single_value('Accounts Settings','overdue_controller')
+
     if supplier.billing_limit and role_assigned not in frappe.get_roles(user):
-            if amount[0].grand_total>=supplier.billing_limit :
+            if query[0].grand_total>=supplier.billing_limit :
                 frappe.throw("You cannot Submit due to Billing Limit")
     
--- credit_limit_control/events/purchase_order.py
+++ credit_limit_control/events/purchase_order.py
@@ -1,22 +1,22 @@
 import frappe
 
 def before_submit(doc, method):
-    query="""
+    query=frappe.db.sql(f"""
     SELECT
         po.supplier AS supplier,
         SUM(po.grand_total) AS grand_total
     FROM
         `tabPurchase Order` AS po
     WHERE
-        po.docstatus=1
+        po.docstatus=1 AND po.supplier = '{doc.supplier}'
     GROUP BY
         po.supplier
-    """
-    amount= frappe.db.sql(f"{query}", as_dict=True)
-    supplier = frappe.get_doc('Supplier', doc.supplier)
+    """, as_dict = True)
 
+    supplier = frappe.get_doc('Supplier', doc.supplier)
     user = frappe.session.user
     role_assigned=frappe.db.get_single_value('Accounts Settings','credit_controller')
+
     if supplier.credit_limit and role_assigned not in frappe.get_roles(user):
-            if amount[0].grand_total>=supplier.credit_limit :
+            if query[0].grand_total>=supplier.credit_limit :
                 frappe.throw("You cannot Submit due to Credit Limit")
